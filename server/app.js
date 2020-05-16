d20a7f44e420a9d1b9d7d4ef17c4dc3849552fb0
d.shioya <d.shioya@wiz-net.jp>
2020-05-16 17:05:55 +0900

    モックサーバを組み込み

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 8080
+  port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(history())
-app.use(serveStatic(__dirname + '/../dist/spa'))
+app.use('/api', require('./api'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(history())
+  // eslint-disable-next-line no-path-concat
+  app.use(serveStatic(__dirname + '/../dist/spa'))
+}
+
 app.listen(port)
